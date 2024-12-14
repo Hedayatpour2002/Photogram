116fc15a3cbf8c8a70ad6e6fdb527f0e118df8ef
Ahmad Hedayatpour <ahmadhedayatpour2002@gmail.com>
2024-12-14 21:26:39 +0330

    Add Splash

--- app/src/main/java/com/example/photogram/MainActivity.kt
+++ app/src/main/java/com/example/photogram/MainActivity.kt
@@ -1,36 +1,79 @@
 package com.example.photogram
 
+import android.graphics.drawable.Animatable
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
 import androidx.navigation.compose.rememberNavController
 import com.example.photogram.ui.theme.PhotogramTheme
-
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            val navController = rememberNavController()
+            var showSplash by remember { mutableStateOf(true) }
+
             PhotogramTheme {
-                Scaffold(modifier = Modifier.fillMaxSize(), bottomBar = {
-                    BottomNavigationBar(navController = navController)
-                }
-                ) { innerPadding ->
-                    NavigationHost(
-                        navController = navController,
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                if (showSplash) {
+                    SplashScreen {
+                        showSplash = false
+                    }
+                } else {
+                    val navController = rememberNavController()
+                    Scaffold(
+                        modifier = Modifier.fillMaxSize(),
+                        bottomBar = {
+                            BottomNavigationBar(navController = navController)
+                        }
+                    ) { innerPadding ->
+                        NavigationHost(
+                            navController = navController,
+                            modifier = Modifier.padding(innerPadding)
+                        )
+                    }
                 }
             }
         }
     }
 }
 
+@Composable
+fun SplashScreen(onAnimationEnd: () -> Unit) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        AndroidView(
+            modifier = Modifier.fillMaxSize(),
+            factory = { ctx ->
+                ImageView(ctx).apply {
+                    setImageDrawable(ContextCompat.getDrawable(ctx, R.drawable.photogram_anim))
+                    (drawable as? Animatable)?.start()
+                }
+            }
+        )
+    }
 
+    LaunchedEffect(Unit) {
+        delay(1000)
+        onAnimationEnd()
+    }
+}
